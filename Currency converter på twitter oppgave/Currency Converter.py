50d4843f66ad1a23b44eeaeaad51689a6d22e39d
DanielStroem <92923306+DanielStroem@users.noreply.github.com>
2023-05-04 12:59:09 +0200

    Update Currency Converter.py
    Oppdaterte Currency Converter fordi forex python ikke ville finne hva siste rates var, så fikset det til at nå så søker du og på datoen i dag for å finne de siste rates-ene.

--- Currency converter på twitter oppgave/Currency Converter.py
+++ Currency converter på twitter oppgave/Currency Converter.py
@@ -1,5 +1,5 @@
 from forex_python.converter import CurrencyRates
-from datetime import datetime
+import datetime
 import schedule
 import tweepy
 
@@ -11,7 +11,7 @@ api=tweepy.API(auth)
 def job():
     #Finner ut hvor mye 1 USD er i NOK, og runder opp til 5 desimaler
     currency=CurrencyRates()
-    norskeKronen=round(currency.get_rate('USD', 'NOK'),5)
+    norskeKronen=round(currency.get_rate('USD', 'NOK', date), 5)
 
     #Leser øverste linje i EnUsdSist.txt filen og vi setter øverste linjen i en float variable
     read=open('EnUsdSist.txt','r')
@@ -50,8 +50,9 @@ schedule.every().day.at("15:00").do(job)
 
 while True:
     #Finner hvilken dag det er 0 er mandag 6 er søndag
-    x=datetime.now()
+    x=datetime.datetime.now()
     dag=x.weekday()
+    date=datetime.date.today()
     
     #Må ha "and" her fordi hvis begge er usanne så vil programmet gjøre job()
     #"or" vil gjøre job() hvis bare en av påstandene er sanne, så den ville da enda poste på lørdag og søndag
